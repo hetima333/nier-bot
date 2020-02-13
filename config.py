09cf51dc2c710f407ba3a3479ae6f9b489a59240
hetima333 <hetima333@gmail.com>
2020-02-13 20:50:07 +0900

    [add]最初のコミット

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+# 公開可能な設定を記述する
+COMMAND_PREFIX = '!!'
+DEFAULT_EMBED_COLOR = 0x8080c0
---
+++ main.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from discord.ext import commands
+import discord
+import os
+import traceback
+import setting
+import extentions
+import config
+
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
+
+
+# Botクラス
+class Bahamut(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix=config.COMMAND_PREFIX,
+            description="",
+            fetch_offline_members=False,
+        )
+
+        # 拡張機能の読み込み
+        for extention in extentions.extentions:
+            try:
+                self.load_extension(f'cogs.{extention}')
+            except Exception:
+                traceback.print_exc()
+
+    # 起動時のイベント
+    async def on_ready(self):
+        print(f'Ready: {self.user} (ID: {self.user.id})')
+        activity = discord.Game('神撃、究極の竜HL')
+        await self.change_presence(activity=activity)
+
+    # Botを起動させる
+    def run(self):
+        super().run(TOKEN)
+
+
+# Botの作成
+bot = Bahamut()
+
+# Botの起動
+if __name__ == '__main__':
+    bot.run()
