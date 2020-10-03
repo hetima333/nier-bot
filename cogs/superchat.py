9a11595cd854bbf355d1abb77f195f463f79f842
hetima333 <hetima333@gmail.com>
2020-10-03 15:35:40 +0900

    [update]unicodeカラー絵文字に対応

--- cogs/superchat.py
+++ cogs/superchat.py
@@ -8,15 +8,18 @@ import random
 import os
 import re
 import emoji
+import base64
 
 from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
+from .utils.emoji_converter import EmojiConverter
 
 
 class SuperChat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.color_file = Path("data/json/superchat_data.json")
+        self.converter = EmojiConverter()
 
     @commands.command()
     async def sc(self, ctx, message: str):
@@ -96,7 +99,9 @@ class SuperChat(commands.Cog):
         format_msg = re.sub('@\n@', '@@', format_msg)
 
         lines = format_msg.count(os.linesep)
-        height = 150 + lines * 22
+        text_height = 22
+        text_width = 22
+        height = 150 + lines * text_height
 
         im = Image.new("RGBA", (450, height), tuple(main_color))
         draw = ImageDraw.Draw(im)
@@ -117,31 +122,39 @@ class SuperChat(commands.Cog):
         offset = [0, 0]
         prev_str = ''
         for i, s in enumerate(format_msg):
+            # 絵文字の場合はbase64に変換して画像化、文字の代わりに埋め込み
             if s in emoji.UNICODE_EMOJI:
-                draw.text((20 + offset[0], 115 + offset[1]),
-                          s, fill=tuple(text_color), font=emoji_font)
-                offset[0] += 20
+                emoji_str = self.converter.to_base64_png(s)
+                imgdata = base64.b64decode(str(emoji_str))
+                emoji_img = Image.open(io.BytesIO(imgdata)).convert('RGBA')
+                emoji_img = emoji_img.resize((22, 22), Image.BICUBIC)
+                im.paste(
+                    emoji_img, (20 + offset[0], 115 + offset[1]), emoji_img.split()[3])
+                offset[0] += text_width
             elif unicodedata.east_asian_width(s) in 'FWA':
                 draw.text((20 + offset[0], 115 + offset[1]),
                           s, fill=tuple(text_color), font=text_font)
-                offset[0] += 22
+                offset[0] += text_width
             else:
                 draw.text((20 + offset[0], 115 + offset[1]),
                           s, fill=tuple(text_color), font=text_font)
-                offset[0] += 11
+                offset[0] += int(text_width / 2)
 
+            # カスタム絵文字を画像に置換
             if s == '@' and prev_str == '@':
                 pos = [20 + offset[0] - 22, 115 + offset[1]]
-                draw.rectangle((pos[0], pos[1], pos[0] + 50, pos[1] + 25), fill=tuple(back_color))
+                draw.rectangle((pos[0], pos[1], pos[0] + 50,
+                                pos[1] + 25), fill=tuple(back_color))
                 data = io.BytesIO(await stamp_list.pop(0).read())
                 stamp_img = Image.open(data).convert('RGBA')
-                stamp_img = stamp_img.resize((20, 20), Image.BICUBIC)
-                im.paste(stamp_img, (pos[0], pos[1]), stamp_img.split()[3])
+                stamp_img = stamp_img.resize((22, 22), Image.BICUBIC)
+                im.paste(stamp_img, (pos[0] - 2,
+                                     pos[1] - 2), stamp_img.split()[3])
             prev_str = s
 
             if s == '\n':
                 offset[0] = 0
-                offset[1] += 22
+                offset[1] += text_height
 
         # 画像合成
         thum = thum.resize((60, 60), Image.BICUBIC)
