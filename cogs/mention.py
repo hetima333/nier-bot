8afe5a7a41807d9ba83fb07e11544c09415cb1fe
hetima333 <hetima333@gmail.com>
2020-10-05 17:51:12 +0900

    [update]Apexのクラフト情報を表示できるように

--- cogs/mention.py
+++ cogs/mention.py
@@ -36,6 +36,37 @@ class Mention(commands.Cog):
                 f"{message.author.mention} シートのリンクだ…よ",
                 embed=embed)
 
+        if message.content in ['クラフト', 'くらふと']:
+            # apex_daily_crafting チャンネルから最新のメッセージを取得する
+            craft_channel_id = '762582916348903435'
+            craft_channel = await self.bot.fetch_channel(craft_channel_id)
+
+            last_message_id = craft_channel.last_message_id
+            if last_message_id is None:
+                return
+
+            last_message = await craft_channel.fetch_message(last_message_id)
+            if len(last_message.embeds) == 0:
+                return
+            tweet_embed = last_message.embeds[0]
+            if tweet_embed.image is None:
+                return
+
+            # 最新のメッセージが送信された月日
+            send_date = last_message.created_at
+            date_text = f"{send_date.month}/{send_date.day}"
+            # 該当する画像へのリンク
+            img_link = tweet_embed.image.url
+
+            embed = discord.Embed(color=config.DEFAULT_EMBED_COLOR)
+            embed.set_author(
+                name=f'{date_text}のクラフト内容',
+                icon_url=self.bot.user.avatar_url)
+            embed.set_image(url=img_link)
+            await message.channel.send(
+                f"{message.author.mention} {date_text}のクラフトだ…よ",
+                embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Mention(bot))
