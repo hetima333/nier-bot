c206c8b681ff0718dae334a78880733ff6d3af7e
hetima333 <hetima333@gmail.com>
2021-02-23 18:05:10 +0900

    [fix]クラフト機能の無効化

--- cogs/mention.py
+++ cogs/mention.py
@@ -44,7 +44,7 @@ class Mention(commands.Cog):
                 'https://cdn.discordapp.com/attachments/683536765977624590/734054219178311740/333fc592439f2795bee1906e3d1b90e9.png'
             ]
             embed.set_image(url=random.choice(urls))
-            await message.channel.send(embed=embed)
+            await message.reply(embed=embed)
 
         if "はよいけ" == message.content:
             embed = discord.Embed(color=config.DEFAULT_EMBED_COLOR)
@@ -74,41 +74,6 @@ class Mention(commands.Cog):
             await message.channel.send(
                 f"{message.author.mention} シートのリンクだ…よ",
                 embed=embed)
-
-        if message.content in ['クラフト', 'くらふと']:
-            # apex_daily_crafting チャンネルから最新のメッセージを取得する
-            craft_channel_id = '762582916348903435'
-            craft_channel = await self.bot.fetch_channel(craft_channel_id)
-
-            last_message_id = craft_channel.last_message_id
-            if last_message_id is None:
-                return
-            last_message = await craft_channel.fetch_message(last_message_id)
-            if len(last_message.embeds) == 0:
-                return
-            tweet_embed = last_message.embeds[0]
-            if tweet_embed.image is None:
-                return
-
-            current_date = datetime.datetime.now()
-            send_date = last_message.created_at
-
-            # 3時より前なら1日前の送信まで有効
-            if current_date.day == send_date.day or (current_date.hour < 3 and current_date.day - 1 == send_date.day):
-                date_text = f"{send_date.month}/{send_date.day}"
-                # 該当する画像へのリンク
-                img_link = tweet_embed.image.url
-
-                embed = discord.Embed(color=config.DEFAULT_EMBED_COLOR)
-                embed.set_author(
-                    name=f'{date_text}のクラフト内容',
-                    icon_url=self.bot.user.avatar_url)
-                embed.set_image(url=img_link)
-                await message.channel.send(
-                    f"{message.author.mention} {date_text}のクラフトだ…よ",
-                    embed=embed)
-            else:
-                await message.channel.send('クラフト情報が更新されていないの…ごめんなさい…')
 
 
 def setup(bot):
