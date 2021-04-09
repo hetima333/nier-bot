9efc76b9276764e6406e79cbee3f9ef09e68d952
hetima333 <hetima333@gmail.com>
2021-04-09 16:25:43 +0900

    [update]エアシップマップの追加

--- cogs/amongus.py
+++ cogs/amongus.py
@@ -1,3 +1,7 @@
+import json
+import re
+from pathlib import Path
+
 from discord.ext import commands
 import discord
 
@@ -7,32 +11,25 @@ import config
 class AmongUs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.map_data = {
-            'theskeld': {
-                'img_url': 'https://cdn.discordapp.com/attachments/500215831784062977/813701787503820820/El3tBPkU8AA8iQE.png',
-                'formal_name': 'The Skeld'
-            },
-            'mirahq': {
-                'img_url': 'https://cdn.discordapp.com/attachments/500215831784062977/813701836803670016/El3qJKJU4AUHPDP.png',
-                'formal_name': 'MIRA HQ'
-            },
-            'polus': {
-                'img_url': 'https://cdn.discordapp.com/attachments/500215831784062977/813701880454053928/El3qJJkVMAATYAb.png',
-                'formal_name': 'Polus'
-            }
-        }
+        self.MAP_DATA_FILE = Path('data/json/amongus.json')
 
     @commands.command()
     async def au(self, ctx, map_name: str):
-        _map_name = map_name.lower()
-        _map_name = _map_name.replace(' ', '')
-        map_data = self.map_data.get(_map_name)
-        if map_data is None:
-            await ctx.message.reply(f"{map_name} という名前のマップが見つからないわ…ごめんなさい")
-        embed = discord.Embed(color=config.DEFAULT_EMBED_COLOR)
-        embed.set_image(url=map_data['img_url'])
-        await ctx.message.reply(
-            f"{map_data['formal_name']}のマップだ…よ", embed=embed)
+        with self.MAP_DATA_FILE.open() as f:
+            map_data = json.loads(f.read())
+
+        for v in map_data:
+            r = re.fullmatch(f"{v['reg']}", map_name, re.I)
+
+            if r is not None:
+                embed = discord.Embed(color=config.DEFAULT_EMBED_COLOR)
+                embed.set_image(url=v['img_url'])
+                await ctx.message.reply(
+                    f"{v['formal_name']}のマップだ…よ", embed=embed)
+
+                return
+
+        await ctx.message.reply(f"{map_name} という名前のマップが見つからないわ…ごめんなさい")
 
 
 def setup(bot):
